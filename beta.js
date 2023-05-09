eddeeaf53aa4b3d0e3675ae0d4b7ff99ff1912fb
maruizz-p <marioruizp00@gmail.com>
2023-05-09 17:16:25 +0200

    Beta

---
+++ beta.js
@@ -0,0 +1,47 @@
+// Variables
+let variable1 = 'valor';
+let variable2 = 10;
+
+// Constantes
+const constante1 = 'valor constante';
+const constante2 = 5;
+// Función
+
+function nombreFuncion() 
+    {
+    // Código de la función
+    }
+  
+  // Función de flecha
+  const nombreFuncion = () => 
+  {
+    // Código de la función
+  }
+  document.addEventListener('DOMContentLoaded', () => 
+  {
+    // Código para manipulación del DOM
+  });
+  function sumar(a, b) {
+    return a + b;
+  }
+  
+  const resultado = sumar(variable2, constante2);
+  console.log(resultado);
+// Llamada a una API
+fetch('https://api.example.com/data')
+  .then(response => response.json())
+  .then(data => 
+    {
+    // Manejo de los datos de la API
+    })
+  .catch(error => 
+    {
+    // Manejo de errores
+    });
+  export function myFunction() 
+  {
+    // Código de la función
+  }
+  
+  export const myVariable = 'valor';
+      
